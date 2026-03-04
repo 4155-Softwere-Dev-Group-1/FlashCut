b4ab55b5a006e049f32d1beed2eff25602c607af
Tyler Weber <tweber13@charlotte.edu>
2026-03-04 18:13:05 -0500

    set up database connection and config

--- server/src/app.js
+++ server/src/app.js
@@ -1,18 +1,14 @@
-// Express app initialization
-
 const express = require('express');
+const cors = require('cors');
 const flashcardRoutes = require('./routes/flashcardRoutes');
 
 const app = express();
 
-// Middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
 app.use('/api/flashcards', flashcardRoutes);
-
-// Error handling middleware
 app.use(require('./middleware/errorHandler'));
 
 module.exports = app;
