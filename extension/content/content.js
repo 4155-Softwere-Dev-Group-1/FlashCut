87749831db2f993c321e2e7365368fd65a270d4d
hunterbeachum <hunterbeachum@gmail.com>
2026-03-27 13:19:29 -0400

    change extension endpoint to port 5000 and /api/flashcards route

--- extension/content/content.js
+++ extension/content/content.js
@@ -9,7 +9,7 @@ document.addEventListener('mouseup', async () => {
     
     // Send to backend API
     try {
-      const response = await fetch('http://localhost:3000/api/captures', {
+      const response = await fetch('http://localhost:5000/api/flashcards', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
